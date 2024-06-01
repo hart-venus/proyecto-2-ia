cdc3e909ac1dd6ead407e56fffbc69bb0a6c6763
Ariel Leyva <aleyva1509@gmail.com>
2024-06-01 15:19:50 -0600

    add: main call to RagApi, find documents from directory

--- RagApi.py
+++ RagApi.py
@@ -1,5 +1,23 @@
+import os
+import sys 
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) # Add parent directory to path variable
 
 class RagApi: 
     def __init__(self, docs_dir='docs'):
         self.docs_dir = docs_dir
+        docs = self.find_docs(docs_dir, '.pdf') # find all pdf files in docs dir
+        print(docs)
+
+
+    def find_docs(self, docs_dir, ext):
+        docs = []
+        for root, dirs, files in os.walk(docs_dir):
+            for file in files:
+                if file.endswith(ext):
+                    docs.append(os.path.join(root, file))
+            # recursive call to find_docs
+            for dir in dirs:
+                docs += self.find_docs(dir, ext)
+        return docs
 
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+from RagApi import RagApi
+
 if __name__ == '__main__':
-    print('Hello, World!')
+    ra = RagApi()
