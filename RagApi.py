9da445a1fa56b52073040045a81e6e02ee08b35f
Ariel Leyva <aleyva1509@gmail.com>
2024-06-01 15:53:01 -0600

    add: chunk splitting

--- RagApi.py
+++ RagApi.py
@@ -1,6 +1,7 @@
 import os
 import sys 
 from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..')) # Add parent directory to path variable
 
@@ -8,8 +9,15 @@ class RagApi:
     def __init__(self, docs_dir='docs'):
         self.docs_dir = docs_dir
         docs = self.find_docs(docs_dir, '.pdf') # find all pdf files in docs dir
-        pages = self.load_docs(docs)
-        print(len(pages))
+        pages = self.load_docs(docs) # loads all pages from pdf files
+        
+        # Split pages into chunks (paragraphs) for better semantic encoding
+        r_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=450, # max number of characters in a chunk
+            chunk_overlap=50, # number of characters to overlap between chunks
+            separators=["\n\n", "\n", " ", ""]
+        )
+        chunks = r_splitter.split_documents(pages)
 
     def load_docs(self, docs): 
         # from an array of pdf files, load all the pages with langchain 
